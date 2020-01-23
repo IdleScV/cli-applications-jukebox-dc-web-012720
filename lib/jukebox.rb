96144b2469ffa438d9315161507d9552c288e7f4
Wesley Chen <weschen1996@gmail.com>
2020-01-23 18:58:12 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,67 +1,67 @@
 # Add your code here
-intro = "Please enter a command:"
 
-songs = [
-  "Phoenix - 1901",
-  "Tokyo Police Club - Wait Up",
-  "Sufjan Stevens - Too Much",
-  "The Naked and the Famous - Young Blood",
-  "(Far From) Home - Tiga",
-  "The Cults - Abducted",
-  "Phoenix - Consolation Prizes",
-  "Harry Chapin - Cats in the Cradle",
-  "Amos Lee - Keep It Loose, Keep It Tight"
-]
 
-def options(input)
-  case input
-    when "help"
-      
-      puts "I accept the following commands:"
-      puts "- help : displays this help message"
-      puts "- list : displays a list of songs you can play"
-      puts "- play : lets you choose a song to play"
-      puts "- exit : exits this program"
-      
-      puts intro
-      input = gets.chomp
-      options(input)
-    when "list"
-      
-      count = 0
-        while count < songs.length
-          puts (count+1).to_s + ". " + songs[count]
-          count += 1
-        end
-        
-      puts intro
-      
-      input = gets.chomp
-      options(input)
-      
-    when "play"
-      
-      songinput = gets.chomp
-      
-        if (1..9).to_a.include? songinput.to_i
-          puts "Playing " + songs[count]
-        elsif songs.include? songinput
-          puts "Playing " + songinput 
-        end  
-        
-      puts intro
-      input = gets.chomp
-      options(input)
-    when "exit"
-      
-      puts "goodbye"
-      return
-      
-  end
+def intro
+    puts "Please enter a command:"
+    input = gets.chomp
 end
 
-puts intro
-input = gets.chomp
-options(input)
+def help
+    puts "I accept the following commands:"
+    puts "- help : displays this help message"
+    puts "- list : displays a list of songs you can play"
+    puts "- play : lets you choose a song to play"
+    puts "- exit : exits this program"
+end
+
+def list(arr)
+    arr.each_with_index {|x, index| puts (index + 1).to_s + ". " + x}
+end
+
+def play(input, arr)
     
+    if (1..arr.length).include? input.to_i
+        puts arr[input.to_i]
+    elsif arr.include? input
+        puts input
+    end
+
+end
+
+def exit
+    puts "goodbye"
+    return
+end
+      
+def run(intro)
+    songs = [
+        "Phoenix - 1901",
+        "Tokyo Police Club - Wait Up",
+        "Sufjan Stevens - Too Much",
+        "The Naked and the Famous - Young Blood",
+        "(Far From) Home - Tiga",
+        "The Cults - Abducted",
+        "Phoenix - Consolation Prizes",
+        "Harry Chapin - Cats in the Cradle",
+        "Amos Lee - Keep It Loose, Keep It Tight"
+      ]
+    
+    case input
+    when "help"
+        help
+        command(intro)
+    when "list"
+        list(songs)
+        command(intro)
+    when "play"
+        puts "Enter a song or its ID number"
+        songinput = gets.chomp
+        play(songinput, songs)
+        command(intro)
+    when "exit"
+        exit
+    end
+end
+
+
 
